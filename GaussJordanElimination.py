4472ba9533f228a437af83099917d7d2d35293b2
dennis <dennis.alacahanli@revsolz.com>
2025-07-06 19:06:25 -0600

    starting

--- GaussJordanElimination.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-import math
-
-def greatest_common_factor(numbers:list):
-    if type(numbers) != list:
-        raise TypeError("This data type is not accepted")
-    return reduce(math.gcd, numbers)
-
-
